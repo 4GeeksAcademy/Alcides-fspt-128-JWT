4a9a87e6598ba9a6e2d99b938905078a5b051b67
Alcides Rivera <elarsi626@gmail.com>
2026-02-11 17:35:03 +0000

    L

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-export const Navbar = () => {
+ export const Navbar = () => {
 
 	return (
 		<nav className="navbar navbar-light bg-light">
---
+++ src/front/services/BackendServices.js
@@ -0,0 +1,14 @@
+export const login = async (user) => {
+    const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/login`, {
+        method: "POST",
+        body: JSON.stringify(user),
+        headers: {
+            "Content-type": "application/json"
+        }
+    })
+    const data = await response.json()
+
+    if(!response.ok){
+        alert(data.error)
+    }
+    }
