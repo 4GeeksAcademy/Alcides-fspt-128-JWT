1102dd185db0a4694988fbd0039b4e5e0f2adc0c
Alcides Rivera <elarsi626@gmail.com>
2026-02-11 16:31:03 +0000

    First

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,6 +5,7 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_jwt_extended import create_access_token, get_jwt_identity
 
 api = Blueprint('api', __name__)
 
@@ -20,3 +21,45 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/signup', methods=['POST'])
+def signup():
+    data = request.get_json()
+    email = data.get('email')
+    password = data.get('password')
+    if not email or not password:
+        return jsonify({"error": "Email and password are required"}), 400
+    existing_user = db.session.execute(db.select(User).where(
+        User.email == email)).scalar_one_or_none()
+    if existing_user:
+        return jsonify({"error": "User already exists"}), 409
+    new_user = User(email=email)
+    new_user.set_password(password)
+
+    db.session.add(new_user)
+    db.session.commit()
+
+
+    return jsonify({"msg": "User created succesfully"}), 201
+
+
+@api.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    email = data.get('email')
+    password = data.get('password')
+    if not email or not password:
+        return jsonify({"error": "Email and password are required"}), 400
+    
+    user = db.session.execute(db.select(User).where(
+        User.email == email)).scalar_one_or_none()
+    
+    if user is None:
+        return jsonify({"error": "Invalid email or password"}), 401
+    
+    if user.check_password(password):
+        access_token = create_access_token(identity=str(user.id))
+        return jsonify({"msg": "Login succesful", "token": access_token, "user": user.serialize()}), 200
+    else:
+        return jsonify({"error": "Invalid email or password"}), 401
+
